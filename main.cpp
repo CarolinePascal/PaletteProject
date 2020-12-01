13e8109e43c649e86d65f30a99f2acd9f8e0fd1f
CarolinePascal <caroline8.pascal@gmail.com>
2020-12-01 19:29:15 +0100

    Adding command line parameters parsing, palettre generation test and display

--- main.cpp
+++ main.cpp
@@ -1,12 +1,48 @@
 #include <iostream>
+#include <thread>
+
 #include "Picture.h"
+#include "KMeans.h"
  
 int main( int argc, char** argv ) {
+
+    //Parameters parsing - Name of the picture file, number of colors within the palette
+
+    String fileName = "";
+    int colorNumber = 5;
+
+    switch (argc)
+    {
+    case 1:
+        throw("No file name given !");
+
+    case 2:
+        fileName = argv[1];
+        break;
+
+    case 3:
+        fileName = argv[1];
+        colorNumber = stoi(argv[2]);
+        break;
     
-    Picture picture = Picture("Square.jpg");
-    picture.Display();
+    default:
+        throw("Too many parameters given !");
 
-    cout<<picture.GetRGB(20,20)<<endl;
+    }
 
+    //Initialize random seed: 
+    srand (time(NULL));
+
+    //Display picture TODO Internalize thread
+    Picture picture = Picture(fileName);
+    thread thread_picture(&Picture::Display,&picture);
+
+    //Compute and display palette TODO Internalize thread
+    vector<Vec3i> test = KMeans(picture, colorNumber, 10);
+    thread thread_palette(DisplayPalette,test);
+    
+    thread_picture.join();
+    thread_palette.join();
+    
     return(1);
 }
